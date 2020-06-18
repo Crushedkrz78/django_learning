c811f415591372755f2304fcee62224855a1e412
Crushedkrz 78 <matrix_kriztyan@live.com.mx>
2020-06-18 17:21:19 -0500

    Setting Index Template and configuration

--- libro/applications/home/views.py
+++ libro/applications/home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import(
+    TemplateView,
+)
+
+class IndexView(TemplateView):
+    # Una vista procesa los datos y renderiza resultado en pantalla
+    # Siempre se solicitara un Template
+    # Un template es un archivo HTML
+    template_name = "home/index.html"
+    print("Home View Works!")
+
