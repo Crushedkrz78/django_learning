34e06d792504a4f83df68be2fbbd98e2b7d79ffc
Crushedkrz 78 <matrix_kriztyan@live.com.mx>
2020-06-23 12:32:44 -0500

    Proyecto Basico Pt.1

--- libro/applications/biblioteca/admin.py
+++ libro/applications/biblioteca/admin.py
@@ -3,5 +3,28 @@ from django.contrib import admin
 # Register your models here.
 from .models import Autor, Libros
 
-admin.site.register(Autor)
-admin.site.register(Libros)
+#Improving Authon Class
+class AutorAdmin(admin.ModelAdmin):
+    list_display = (
+        'nombre',
+        'nacionalidad',
+        'id'
+    )
+    #Field Search attribute
+    search_fields = ('nombre', 'nacionalidad',)
+
+#Improving Authon Class
+class LibrosAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'resume',
+        'autor',
+        'id'
+    )
+    #Field Search attribute
+    search_fields = ('title',)
+    #Filter Attribute
+    list_filter = ('autor',)
+
+admin.site.register(Autor, AutorAdmin)
+admin.site.register(Libros, LibrosAdmin)
